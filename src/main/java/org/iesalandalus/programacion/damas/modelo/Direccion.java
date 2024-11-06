3b4d71588a0eedf8049e2cb7224ae66d83e35964
Michael <miguelsf12@hotmail.es>
2024-11-06 19:29:06 +0100

    Commit direccion

---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -0,0 +1,20 @@
+// Direccion.java
+package org.iesalandalus.programacion.damas.modelo;
+
+public enum Direccion {
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste"),
+    NOROESTE("Noroeste");
+
+    private String cadenaAMostrar;
+
+    Direccion(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return cadenaAMostrar;
+    }
+}
